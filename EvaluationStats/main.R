210b3c2d67f81d35029de16a1164aa21d9d9f8e6
amrshurman <amrshurman@cmail.carleton.ca>
2019-03-16 22:34:10 -0400

    added histograms for total,successful,failed

--- EvaluationStats/main.R
+++ EvaluationStats/main.R
@@ -1,3 +1,10 @@
 mydata <- read.csv("../data/loginData.csv")
+attach(mydata)
 print(mydata)
+
+hist(Logins.Total, ylim=c(0,20),breaks=7, main="Histogram of Total Logins", xlab = "Total logins")
+
+hist(Logins.success, ylim=c(0,25), xlim=c(0,25), main="Histogram of Succesful Logins", xlab = "Succesful logins")
+
+hist(Logins.Failure, ylim=c(0,25), xlim=c(0,25), main="Histogram of Failed Logins", xlab = "Failed logins")
 
