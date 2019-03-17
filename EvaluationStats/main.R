30a954ba88ec3976bc82fd39f1b9bec1fd19585b
amrshurman <amrshurman@cmail.carleton.ca>
2019-03-17 18:31:47 -0400

    added print statements

--- EvaluationStats/main.R
+++ EvaluationStats/main.R
@@ -2,26 +2,42 @@ mydata <- read.csv("../data/loginData.csv")
 attach(mydata)
 print(mydata)
 
+print("Mean for total login")
 print(mean(Logins.Total))
+print("Mean for successful login")
 print(mean(Logins.success))
+print("Mean for Failed login")
 print(mean(Logins.Failure))
 
+print("Median for total login")
 print(median(Logins.Total))
+print("Median for successful login")
 print(median(Logins.success))
+print("Median for Failed login")
 print(median(Logins.Failure))
 
 print(sd(Logins.Total))
+print("sd for Total login")
 print(sd(Logins.success))
+print("sd for Successful login")
 print(sd(Logins.Failure))
+print("sd for Failed login")
 
+print("Mean for successful login time")
 print(mean(Logins.avgSuccess))
+print("Mean for failed login time")
 print(mean(Logins.avgFail))
 
+print("Median for successful login time")
 print(median(Logins.avgSuccess))
+print("Median for failed login time")
 print(median(Logins.avgFail))
 
+print("sd for successful login time")
 print(sd(Logins.avgSuccess))
+print("sd for failed login time")
 print(sd(Logins.avgFail))
+
 
 hist(Logins.Total, ylim=c(0,20),breaks=7, main="Histogram of Total Logins", xlab = "Total logins")
 
